dd487b878fa05669eafc21e94fb9e041ea6e03e8
Skafishka <sergey.kolygin@gmail.com>
2022-08-09 01:14:07 +0300

    added column view and improved filtering function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,46 +7,82 @@ const Journeys = (props) => {
   if (props.month === 1) {
     return (
       <>
-        <h4>Filter by station departure name: </h4> <input value={props.showFiltered} onChange={props.handleFilterChange}/>
+        <h4>Filter by station's departure or return name: </h4> <input value={props.showFiltered} onChange={props.handleFilterChange}/>
         <h4>Here there are:</h4>
-        {props.journeysMay.filter(station => station["Departure station name"].toLowerCase()
-          .includes(props.showFiltered.toLowerCase()))
-          .map((value, id) => (
-            <li key={id.toString()}>
-              <b>Departure: </b> {value["Departure station name"]} <b>Return: </b>{value["Return station name"]} <b>Duration: </b> {moment.duration(value["Duration (sec)"], "seconds").format("mm:ss")} min <b>Distance: </b> {value["Covered distance (m)"] / 1000} km
-            </li>
-          ))
-        }
+        <table>
+          <tbody>
+            {props.journeysMay.filter(station => station["Departure station name"].toLowerCase()
+              .includes(props.showFiltered.toLowerCase()))
+              .map((value, id) => (
+                <tr key={id.toString()}>
+                  <td><b>Departure: </b>{value["Departure station name"]}</td><td><b>Return: </b>{value["Return station name"]}</td><td><b>Duration: </b>{moment.duration(value["Duration (sec)"], "seconds").format("mm:ss")} min</td><td><b>Distance: </b>{value["Covered distance (m)"] / 1000} km</td>
+                </tr>
+              ))
+            }
+            {props.journeysMay.filter(station => station["Return station name"].toLowerCase()
+              .includes(props.showFiltered.toLowerCase()))
+              .map((value, id) => (
+                <tr key={id.toString()}>
+                  <td><b>Departure: </b>{value["Departure station name"]}</td><td><b>Return: </b>{value["Return station name"]}</td><td><b>Duration: </b>{moment.duration(value["Duration (sec)"], "seconds").format("mm:ss")} min</td><td><b>Distance: </b>{value["Covered distance (m)"] / 1000} km</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </>
     )
   } else if (props.month === 2) {
     return (
       <>
-        <h4>Filter by station departure name: </h4> <input value={props.showFiltered} onChange={props.handleFilterChange}/>
+        <h4>Filter by station's departure or return name: </h4> <input value={props.showFiltered} onChange={props.handleFilterChange}/>
         <h4>Here there are:</h4>
-        {props.journeysJune.filter(station => station["Departure station name"].toLowerCase()
-          .includes(props.showFiltered.toLowerCase()))
-          .map((value, id) => (
-            <li key={id.toString()}>
-              <b>Departure: </b> {value["Departure station name"]} <b>Return: </b>{value["Return station name"]} <b>Duration: </b> {moment.duration(value["Duration (sec)"], "seconds").format("mm:ss")} min <b>Distance: </b> {value["Covered distance (m)"] / 1000} km
-            </li>
-          ))
-        }
+        <table>
+          <tbody>
+            {props.journeysJune.filter(station => station["Departure station name"].toLowerCase()
+              .includes(props.showFiltered.toLowerCase()))
+              .map((value, id) => (
+                <tr key={id.toString()}>
+                  <td><b>Departure: </b> {value["Departure station name"]}</td><td><b>Return: </b>{value["Return station name"]}</td><td><b>Duration: </b> {moment.duration(value["Duration (sec)"], "seconds").format("mm:ss")} min</td><td><b>Distance: </b> {value["Covered distance (m)"] / 1000} km</td>
+                </tr>
+              ))
+            }
+            {props.journeysJune.filter(station => station["Return station name"].toLowerCase()
+              .includes(props.showFiltered.toLowerCase()))
+              .map((value, id) => (
+                <tr key={id.toString()}>
+                  <td><b>Departure: </b> {value["Departure station name"]}</td><td><b>Return: </b>{value["Return station name"]}</td><td><b>Duration: </b> {moment.duration(value["Duration (sec)"], "seconds").format("mm:ss")} min</td><td><b>Distance: </b> {value["Covered distance (m)"] / 1000} km</td>
+                </tr>
+              ))
+            }  
+          </tbody>
+        </table>
       </>
     )
   } else if (props.month === 3) {
     return (
       <>
-        <h4>Filter by station departure name: </h4> <input value={props.showFiltered} onChange={props.handleFilterChange}/>
+        <h4>Filter by station's departure or return name:</h4> <input value={props.showFiltered} onChange={props.handleFilterChange}/>
         <h4>Here there are:</h4>
-        {props.journeysJuly.filter(station => station["Departure station name"].toLowerCase()
-          .includes(props.showFiltered.toLowerCase()))
-          .map((value, id) => (
-            <li key={id.toString()}>
-              <b>Departure: </b> {value["Departure station name"]} <b>Return: </b>{value["Return station name"]} <b>Duration: </b> {moment.duration(value["Duration (sec)"], "seconds").format("mm:ss")} min <b>Distance: </b> {value["Covered distance (m)"] / 1000} km
-            </li>
-          ))
-        }
+        <table>
+          <tbody>
+            {props.journeysJuly.filter(station => station["Departure station name"].toLowerCase()
+              .includes(props.showFiltered.toLowerCase()))
+              .map((value, id) => (
+                <tr key={id.toString()}>
+                  <td><b>Departure: </b>{value["Departure station name"]}</td><td><b>Return: </b>{value["Return station name"]}</td><td><b>Duration: </b>{moment.duration(value["Duration (sec)"], "seconds").format("mm:ss")} min</td><td><b>Distance: </b>{value["Covered distance (m)"] / 1000} km</td>
+                </tr>
+              ))
+            }
+            {props.journeysJuly.filter(station => station["Return station name"].toLowerCase()
+              .includes(props.showFiltered.toLowerCase()))
+              .map((value, id) => (
+                <tr key={id.toString()}>
+                  <td><b>Departure: </b>{value["Departure station name"]}</td><td><b>Return: </b>{value["Return station name"]}</td><td><b>Duration: </b>{moment.duration(value["Duration (sec)"], "seconds").format("mm:ss")} min</td><td><b>Distance: </b>{value["Covered distance (m)"] / 1000} km</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </>
     )
   }
